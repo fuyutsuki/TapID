5317f81fe90f69c76b27f85c9f9244c0eddb78e9
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2020-02-05 00:51:52 +0900

    解説を追加

--- src/jp/mcbe/TapID/EventListener.php
+++ src/jp/mcbe/TapID/EventListener.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace jp\mcbe\TapID;
 
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\item\ItemIds;
-use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;// 17行目で使用
+use pocketmine\event\player\PlayerInteractEvent;// 26, 28行目で使用
+use pocketmine\event\player\PlayerQuitEvent;// 42行目で使用
+use pocketmine\item\ItemIds;// 31行目で使用
+use pocketmine\utils\TextFormat;// 36行目で使用
 
 /**
  * Class EventListener
@@ -24,9 +24,12 @@ class EventListener implements Listener {
   }
 
   public function onTap(PlayerInteractEvent $ev) {
+    // もしPlayerInteractEventの行動内容が"ブロックを右クリック"なら
     if ($ev->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
       $player = $ev->getPlayer();
+      // もしプレイヤーのインベントリから手に持ってるアイテムのIDを取得して、時計と同じなら
       if ($player->getInventory()->getItemInHand()->getId() === ItemIds::CLOCK) {
+        // もし現在のUnixタイムスタンプが最後にタップしたときのUnixタイムスタンプより大きければ(1秒以上経っていれば)
         if (strtotime("now") > $this->main->getLastTappedTime($player)) {
           $this->main->setLastTappedTime($player);
           $block = $ev->getBlock();
@@ -37,6 +40,7 @@ class EventListener implements Listener {
   }
 
   public function onQuit(PlayerQuitEvent $ev) {
+    // $this->mainに入っているMainクラスのremoveTimer関数にPlayerを渡す
     $this->main->removeTimer($ev->getPlayer());
   }
 
--- src/jp/mcbe/TapID/Main.php
+++ src/jp/mcbe/TapID/Main.php
@@ -1,11 +1,11 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1);// phpの暗黙の型変換を抑制する
 
 namespace jp\mcbe\TapID;
 
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
+use pocketmine\Player;// 30行目で使用
+use pocketmine\plugin\PluginBase;// 14行目で使用
 
 /**
  * Class Main
@@ -17,17 +17,32 @@ class Main extends PluginBase {
   private $timer = [];
 
   public function onEnable() {
+    // EventListenerクラスをイベントのリスナーとして登録する
+    // use文はMainクラスとEventListenerクラスが同じ名前空間内にあるので不要
     $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
   }
 
+  /**
+   * 最後にタップしたときのUnixタイムスタンプを連想配列から返す。なければ0。
+   * @param Player $player
+   * @return int Unixタイムスタンプ
+   */
   public function getLastTappedTime(Player $player): int {
     return $this->timer[$player->getId()] ?? 0;
   }
 
+  /**
+   * 最後にタップしたときのUnixタイムスタンプを連想配列に入れて保存
+   * @param Player $player
+   */
   public function setLastTappedTime(Player $player) {
     $this->timer[$player->getId()] = strtotime("now");
   }
 
+  /**
+   * プレイヤーがログアウトしたときに連想配列からそのキーを持つデータを削除する
+   * @param Player $player
+   */
   public function removeTimer(Player $player) {
     unset($this->timer[$player->getId()]);
   }
